24eeb6f0d9de892d3015e6b4de564cabd1d5b683
claret-alexander1999 <73807716+claret-alexander1999@users.noreply.github.com>
2021-10-22 16:55:29 +1300

    XamarinCalculator
    Xamarin Calculator Complete Project.

---
+++ XamarinCalculator/XamarinCalculator.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+
+namespace XamarinCalculator.Droid
+{
+    [Activity(Label = "XamarinCalculator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
